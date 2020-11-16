02cdc8b229ced137664ad833f81848feebf13cf6
fullmoon-sg <mbfoo7757@gmail.com>
2020-11-16 02:28:38 +0000

    new update

--- script.js
+++ script.js
@@ -11,33 +11,31 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
+
 async function getFlightData(){
-    let response = await axios.get("https://opensky-network.org/api/states/all");
+    let response = await axios.get("https://fullmoon_sg:sfh2298g@opensky-network.org/api/states/all");
     return response.data.states
 }
 //change the aircraft icon from original pointer icon
 const myIcon = L.icon({
-    iconUrl: "images/aircraft.png",
-    iconSize : [50,30],
-    iconAnchor : [25,16]
+iconUrl: "images/aircraft.png",
+iconSize : [80,50],
+iconAnchor : [25,16]
 });
 
 async function addFlightMarkers(map)
 {
 let flight = await getFlightData();
-// let lat = flight[0][6];
-//let long = flight[0][5];
-//console.log(lat,long);
- //   for (let f of flight)
-  //  {
-       // for (let i =0; i < flight.length;i++)
-     //   {
-         L.marker([flight[1][6],flight[1][5]],{icon: myIcon}).addTo(map);
-  
-     //   }
- //   }
-}
 
+  for (i = 0; i < 10; i++){
+         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+         console.log(flight[i][6],flight[i][5]);
+  }
+}
 
 
 addFlightMarkers(map);
