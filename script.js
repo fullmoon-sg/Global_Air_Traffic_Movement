8e6ee2e73c3b63caf0ff987f7db34e40b9dbabf2
fullmoon-sg <mbfoo7757@gmail.com>
2020-11-19 10:05:39 +0000

    Mod nav-bar, include speed and alt display

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let map = L.map('map').setView([0,0], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    minZoom: 4,
+    maxZoom: 10,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -18,7 +19,7 @@ async function getFlightData(){
 //change the aircraft icon from original pointer icon
 const myIcon = L.icon({
 iconUrl: "images/aircraft.png",
-iconSize : [80,50],
+iconSize : [50,40],
 iconAnchor : [25,16]
 });
 
@@ -26,23 +27,21 @@ async function addFlightMarkers(map)
 {
 let flight = await getFlightData();
 //restricted to 10 aircraft to avoid overall the network during development phase
-  for (i = 0; i < 10; i++){
-         L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
-         console.log(flight[i][6],flight[i][5]);
+  for (i = 10; i < 20; i++){
+        L.marker([flight[i][6],flight[i][5]],{icon:myIcon}).addTo(map);
+       
+        console.log(flight[i][6],flight[i][5]);
 
-        let callsign = document.querySelector("#f-Alt");
-       //callsign.value = ((flight[0][13] * 3.3).toFixed(0) + " FT");
-       if (flight[i][6],flight[i][5] == null)
-       {
-         callsign.value = "no information available"
-       }
-       else
-       {
-       callsign.value = ((flight[0][13] * 3.3).toFixed(0) + " Ft");    //convert metres to feet for height
-       }
+        let fAlt = document.querySelector("#f-Alt");
+        let fSpeed = document.querySelector("#f-speed");
+       
+       fAlt.value = ((flight[1][13] * 3.3).toFixed(0) + " Ft");    //convert metres to feet for height
+       fSpeed.value = (((flight[1][13])/10).toFixed(0) + " km/h"); //convert speed to km/h
+
+       console.log(flight[0][10])//Extract aircraft heading
+       
   }
 }
 
 
 addFlightMarkers(map);
-
