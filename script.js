b92ab7f3ec508917a0c244ce43f5ccdef2e5fbea
fullmoon-sg <mbfoo7757@gmail.com>
2020-11-24 14:19:20 +0000

    Create JSON for airport name and description

--- script.js
+++ script.js
@@ -22,8 +22,10 @@ async function addFlightMarkers(map)
  let fNumber = document.querySelector("#f-number");
         let fAlt = document.querySelector("#f-Alt");
         let fSpeed = document.querySelector("#f-speed");
+        let fDirection = document.querySelector("#f-dir");
+        
 //restricted to 10 aircraft to avoid overall the network during development phase
- for (i = 10; i < 20; i++)
+ for (let i = 0; i < 10; i++)
    {   
       let marker = L.marker([flight[i][6],flight[i][5]], {
         icon : myIcon = L.icon({
@@ -31,34 +33,16 @@ async function addFlightMarkers(map)
          iconSize : [50,40],
          iconAnchor : [25,16]
          })}) 
-
-       marker.addTo(map).bindPopup(flight[i][1]);  
-       
-        //Click on any icon will display on the icon flight details into the flight label
+        marker.addTo(map).bindPopup(flight[i][1]);     
+      //Click on any icon will display on the icon flight details into the flight label
        marker.addEventListener('click',function(){ 
         fNumber.value = flight[i][1]; //assign flight number
         fAlt.value = ((flight[i][13] * 3.3).toFixed(0) + " Ft");    //convert metres to feet for height
         fSpeed.value = (((flight[i][9])*3600/1000).toFixed(0) + " km/h"); //convert speed to km/h  
+        fDirection.value = flight[i][10]; //Direction of the aircraft heading
        })   
-
-      //  L.marker([flight[i][6],flight[i][5]], {
-      //   icon : myIcon = L.icon({
-      //    iconUrl: "images/aircraft.png",
-      //    iconSize : [50,40],
-      //    iconAnchor : [25,16]
-      //    })}).addTo(map).bindPopup(flight[i][1]); 
-      //  console.log([flight[i][6],flight[i][5]]);    
-   }  
+   } 
 }
-
-
-
-
-
-
-
-
-
 
 addFlightMarkers(map);
 
@@ -79,7 +63,6 @@ let validation = {};
 let userId = document.querySelector("#name").value;
 let passWord = document.querySelector("#password").value;
 let signIn = document.querySelector("#login")
-
 axios.get("admin/users.json").then(function(response)
 {
     validation = response.data;
